5da9a5663ccf774f4907d3e2a7c1cd28d08ac16b
Harris Geo <harrisgeo1988@gmail.com>
2020-09-12 16:38:19 +0100

    Script for fetching feed and updating readme

---
+++ index.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+import axios from "axios";
+
+const fetchFeed = async () => {
+  const feedRequest = await axios("https://www.harrisgeo.me/feed.json");
+  return feedRequest.data.items.splice(0, 5);
+};
+
+const updateFeed = async () => {
+  try {
+    const readme = fs.readFileSync("./README.md", "utf8");
+
+    const feed = await fetchFeed();
+    const articlesTitle = "## Recent articles:";
+    let updatedReadme = readme.split(articlesTitle)[0] + "\n" + articlesTitle;
+
+    feed.forEach((item) => {
+      updatedReadme += `\n- [${item.title}](${item.url})`;
+    });
+
+    fs.writeFileSync("./README.md", updatedReadme);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+updateFeed();
