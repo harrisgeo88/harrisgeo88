b8c3ec0d30b0276a9df6319952a6b004378201a1
Harris Geo <charilaoss@gmail.com>
2020-11-14 14:33:31 +0000

    do not add new line every time the job runs

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import fs from "fs";
-import axios from "axios";
+import fs from 'fs'
+import axios from 'axios'
 
 const fetchFeed = async () => {
-  const feedRequest = await axios("https://www.harrisgeo.me/feed.json");
-  return feedRequest.data.items.splice(0, 5);
-};
+  const feedRequest = await axios('https://www.harrisgeo.me/feed.json')
+  return feedRequest.data.items.splice(0, 5)
+}
 
 const updateFeed = async () => {
   try {
-    const readme = fs.readFileSync("./README.md", "utf8");
+    const readme = fs.readFileSync('./README.md', 'utf8')
 
-    const feed = await fetchFeed();
-    const articlesTitle = "## Recent articles:";
-    let updatedReadme = readme.split(articlesTitle)[0] + "\n" + articlesTitle;
+    const feed = await fetchFeed()
+    const articlesTitle = '## Recent articles:'
+    let updatedReadme = readme.split(articlesTitle)[0] + articlesTitle
 
     feed.forEach((item) => {
-      updatedReadme += `\n- [${item.title}](${item.url})`;
-    });
+      updatedReadme += `\n- [${item.title}](${item.url})`
+    })
 
-    fs.writeFileSync("./README.md", updatedReadme);
+    fs.writeFileSync('./README.md', updatedReadme)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
-updateFeed();
+updateFeed()
